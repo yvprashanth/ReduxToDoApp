b72aa269333c064e17390dd12a6387f667107441
Prashanth <yvprashanth@gmail.com>
2019-04-17 09:51:46 -0700

    Todos App

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(<Provider store={store}>
                     <App />
